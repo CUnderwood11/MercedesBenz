2ba18bedaadddfb7a91a27666358006f0083dd1e
Colin <colindcallahan@gmail.com>
2024-01-31 21:55:31 -0500

    2nd

---
+++ src/ModelDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const ModelDetails = () => {
+    let { modelName } = useParams();
+
+    // Fetch model details based on the modelName parameter
+
+    return (
+        <div>
+            <h2>Details about {modelName}</h2>
+            {/* Display model details here */}
+        </div>
+    );
+};
+
+export default ModelDetails;
